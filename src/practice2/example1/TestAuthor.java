1852f090238277e6cf181eeb5ff9cb968e1137dc
vera3 <vera3899@gmail.com>
2022-09-09 23:15:52 +0300

    2 прака

---
+++ src/practice2/example1/TestAuthor.java
@@ -0,0 +1,11 @@
+package practice2.example1;
+
+import practice2.example2.Ball;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author author = new Author("Ryzhova Vera", "v@gmail.com", 'w');
+        System.out.println(author);
+
+    }
+}
