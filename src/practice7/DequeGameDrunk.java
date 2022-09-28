746a5f9b6a517f5ddbbc4f4817d094ce6aca4f7d
vera3 <vera3899@gmail.com>
2022-09-28 21:51:38 +0300

    7 практическая работа

---
+++ src/practice7/DequeGameDrunk.java
@@ -0,0 +1,83 @@
+package practice7;
+
+import java.util.ArrayDeque;
+
+import java.util.Scanner;
+
+
+public class DequeGameDrunk {
+    public static void howWinner(ArrayDeque<Integer> pl1, ArrayDeque<Integer> pl2) {
+        int count1 = 0;
+        int count2 = 0;
+        System.out.println(pl1.isEmpty());
+        while (!pl1.isEmpty() || !pl2.isEmpty()) {
+//            System.out.println(0);
+            System.out.println(pl1 + "            " + pl2);
+
+            if (pl1.peek() > pl2.peek()) {
+                if (pl1.peek() == 0 && pl2.peek() == 9) {
+                    pl1.add(pl1.pop());
+                    pl1.add(pl2.pop());
+                    count1++;
+                } else if (pl1.peek() == 9 && pl2.peek() == 0) {
+                    pl2.add(pl2.pop());
+                    pl2.add(pl1.pop());
+                    count2++;
+                } else {
+                    pl1.add(pl1.pop());
+                    pl1.add(pl2.pop());
+                    count1++;
+                }
+
+            } else {
+                if (pl1.peek() == 0 && pl2.peek() == 9) {
+                    pl1.add(pl1.pop());
+                    pl1.add(pl2.pop());
+                    count1++;
+                } else if (pl1.peek() == 9 && pl2.peek() == 0) {
+                    pl2.add(pl2.pop());
+                    pl2.add(pl1.pop());
+                    count2++;
+                } else {
+                    pl2.add(pl2.pop());
+                    pl2.add(pl1.pop());
+                    count2++;
+                }
+            }
+            if (pl1.isEmpty() || pl2.isEmpty()) {
+                break;
+            }
+        }
+        if (count1 > count2) {
+            System.out.println("first " + count1);
+           /* System.out.println(pl1);
+            System.out.println(pl2);
+            pl2.clear();
+            System.out.println(pl1.size()+" "+ pl2.size());
+            System.out.println(pl1);
+            System.out.println(pl2);*/
+        } else {
+            System.out.println("second " + count2);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> player1 = new ArrayDeque<>();
+        ArrayDeque<Integer> player2 = new ArrayDeque<>();
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            if (i < 5) {
+                int temp = scanner.nextInt();
+                player1.push(temp);
+            } else {
+                int temp = scanner.nextInt();
+                player2.push(temp);
+            }
+        }
+        // System.out.println(player1);
+        howWinner(player1, player2);
+//        if(player1.peek()>player2.peek()){
+//
+//        }
+    }
+}
